bea165c63e114856a2d52a2f299457a7ea55c4f5
Thanos Kantaras <92505922+thanosk85@users.noreply.github.com>
2023-09-13 17:56:21 +0300

    continue

--- src/components/SearchResultsItem.tsx
+++ src/components/SearchResultsItem.tsx
@@ -5,19 +5,19 @@ function SearchResultsItem({ post, i }: { post: Post; i: number }) {
 	return (
 		<div className="px-5 py-4 border-gray-300 border-2 rounded-lg flex flex-col lg:flex-row items-center gap-5">
 			{post.featured_image && !!post.featured_image.length && (
-				<div className="rounded-lg">
+				<div className="rounded-lg w-full lg:w-[500px]">
 					<Image
 						src={post.featured_image}
-						width={700}
-						height={400}
+						width={1000}
+						height={1000}
 						alt={post.title}
 						className="rounded-lg"
-						style={{ width: '700px', height: '400px', objectFit: 'cover', objectPosition: 'top' }}
+						style={{ width: '1000px', height: '400px', objectFit: 'cover', objectPosition: 'top' }}
 						priority
 					/>
 				</div>
 			)}
-			<div className="flex flex-col gap-1 grow">
+			<div className="flex flex-col gap-1 w-full lg:w-[calc(100%-500px)]">
 				<h3 className="text-md lg:text-2xl font-semibold text-gray-700 text-center lg:text-left">{post.title}</h3>
 				<p className="text-base lg:text-lg text-gray-700 text-center lg:text-left">{post.excerpt}</p>
 			</div>
