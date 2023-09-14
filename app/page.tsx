a433b4019d0da38debb42a302e716f24f528940b
Thanos Kantaras <92505922+thanosk85@users.noreply.github.com>
2023-09-14 14:56:42 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Search = dynamic(() => import('@/components/Search'));
 const SearchResults = dynamic(() => import('@/components/SearchResults'));
 
 async function getData(params: Params) {
-	const res = await fetch(`https://andko-search.vercel.app/api/posts?search=${params?.search ?? ''}`);
+	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?search=${params?.search ?? ''}`);
 
 	if (!res.ok) {
 		throw new Error('Failed to fetch data');
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { Params } from '@/lib/types';
 
 async function getPost(params: Params) {
-	const res = await fetch(`https://andko-search.vercel.app/api/posts/${params?.slug ?? ''}`);
+	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${params?.slug ?? ''}`);
 
 	if (!res.ok) {
 		throw new Error('Failed to fetch data');
@@ -13,7 +13,7 @@ async function getPost(params: Params) {
 }
 
 export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
-	const res = await fetch(`https://andko-search.vercel.app/api/posts/${params?.slug ?? ''}`);
+	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${params?.slug ?? ''}`);
 
 	if (!res.ok) {
 		throw new Error('Failed to fetch data');
