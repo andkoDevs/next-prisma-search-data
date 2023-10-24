a84f0321de2993a5bed813868dd1dd6679179621
Thanos Kantaras <92505922+thanosk85@users.noreply.github.com>
2023-10-24 12:25:11 +0300

    add dynamic import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from 'next';
+import dynamic from 'next/dynamic';
 import { Inter } from 'next/font/google';
-import '@/styles/globals.css';
-import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
+const Header = dynamic(() => import('@/components/Header'));
 
 export const metadata: Metadata = {
 	title: 'Search Data App',
 	description: 'This is the fastest way to search big data!!!',
 };
+
+import '@/styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,5 +1,7 @@
+import dynamic from 'next/dynamic';
 import { Post, Params } from '@/lib/types';
-import SearchResultsItem from './SearchResultsItem';
+
+const SearchResultsItem = dynamic(() => import('./SearchResultsItem'));
 
 export default function SearchResults({ posts, parameters }: { posts: Post[]; parameters: Params }) {
 	const searchTerm = parameters?.search || 'No search term provided';
@@ -14,11 +16,7 @@ export default function SearchResults({ posts, parameters }: { posts: Post[]; pa
 			)}
 
 			<div className="w-full my-5 flex flex-col gap-5">
-				{!!posts.length ? (
-					posts.map((post: Post, i: number) => <SearchResultsItem key={post.id} post={post} i={i} />)
-				) : (
-					<h3>No Posts found with this keyword!</h3>
-				)}
+				{!!posts.length ? posts.map((post: Post, i: number) => <SearchResultsItem key={post.id} post={post} i={i} />) : <h3>No Posts found with this keyword!</h3>}
 			</div>
 		</section>
 	);
